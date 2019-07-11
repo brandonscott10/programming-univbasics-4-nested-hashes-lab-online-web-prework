985c54c889e7f39d76faeab6ca67d7252db7dfb2
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-11 03:42:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages].first.to_s
-end
+    programmer_hash[:dennis_ritchie][:languages].join
 
 def adding_matz
 # add the following information to the top level of programmer_hash
